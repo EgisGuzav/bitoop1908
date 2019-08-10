26f72b8ab8da7b2adf12a01dbdd595af5201de72
Irmantas <irmantas.radavicius@gmail.com>
2019-08-10 22:25:35 +0300

    Test Smth

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,12 @@
 package app;
 
 public class App {
+
+    public static void test(){
+        System.out.println("Krabas");
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+        App.test();
     }
 }
